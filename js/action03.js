388aa5b6fab82fe55ecaac465ccce57f975c56db
ricardo_maeda_jussi <ricardo_maeda_jussi@whirlpool.com>
2022-03-05 09:09:08 -0300

    fix: I praticed tensor default

--- js/action03.js
+++ js/action03.js
@@ -1,1 +1,21 @@
-﻿
+﻿let textResult = document.querySelector("#result");
+
+// Tensor padrao
+// Esta sera a forma convencional,porque assim nao preciso converter
+// para realizar operações com o tensor
+
+function execute() {
+  const tensorOne = tf.tensor([1, 2, 3, 4]);
+  const tensorTwo = tf.tensor([1, 2, 3, 4], [2, 2]);
+  const tensorThree = tf.tensor([0, 1, 5, 4], [2, 2]);
+
+  //exite a opcao print
+  // vai mostrar no console o valor do tensor
+  tensorOne.print();
+  tensorTwo.print();
+  tensorThree.print();
+
+  textResult.innerHTML += `Tensor padrão  com uma dimensão =>\n${tensorOne.toString()}\n`;
+  textResult.innerHTML += `Tensor padrão  com duas dimensões =>\n${tensorTwo.toString()}\n`;
+  textResult.innerHTML += `Tensor padrão  com duas dimensões e dimensionalidade =>\n${tensorThree.toString()}\n`;
+}
