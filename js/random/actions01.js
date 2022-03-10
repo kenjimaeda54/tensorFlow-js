67446b28ce99804ff957a2131cb1e6e24eddd8a5
ricardo_maeda_jussi <ricardo_maeda_jussi@whirlpool.com>
2022-03-09 21:07:30 -0300

    feat: learned funciton manipulation elment

--- js/random/actions01.js
+++ js/random/actions01.js
@@ -3,7 +3,11 @@ let textResult = document.querySelector("#result");
 function execute() {
   //vai gerar um numero random entre -3 e 3
   //dimensionalidade e 2 por 2
-  const random = tf.randomNormal([2, 2]);
+  const randomNormal = tf.randomNormal([2, 2]);
+  //mesma logica que randomNormal diferença e qeu consigo colocar o valor
+  //mínimo e máximo do numero gerado
+  const randomUniform = tf.randomUniform([2, 2], 1, 10);
 
-  textResult.innerHTML = `Antes do random =>\n${random}\n`;
+  textResult.innerHTML = `RandomNormal =>\n${randomNormal}\n`;
+  textResult.innerHTML += `\nRandomUniform   =>\n${randomUniform}\n`;
 }
